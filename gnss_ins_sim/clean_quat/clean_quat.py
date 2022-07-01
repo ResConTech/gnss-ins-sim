ba4060089da7f4e2241130174af2d50dda52eb6f
hgray2 <hgray576@gmail.com>
2022-07-01 12:02:13 -0400

    cleaning clean_quat.py

--- gnss_ins_sim/clean_quat/clean_quat.py
+++ gnss_ins_sim/clean_quat/clean_quat.py
@@ -7,14 +7,7 @@ __author__ = "Hayden Gray"
 import numpy as np
 from matplotlib import pyplot as plot
 
-#ref_att_quat_file = "sim_results/ref_att_quat.csv"
-
-#ref_quat = np.loadtxt(ref_att_quat_file, delimiter=',', skiprows=1)
-
-#plot.figure()
-#plot.plot(ref_quat)
-
-# Thank you army research laboratory: https://apps.dtic.mil/sti/pdfs/AD1043624.pdf
+# Reference : https://apps.dtic.mil/sti/pdfs/AD1043624.pdf
 def clean(ref_quat):
     clean_quat = np.zeros(ref_quat.shape)
     clean_quat[0] = ref_quat[0]
@@ -27,13 +20,3 @@ def clean(ref_quat):
             clean_quat[i] = ref_quat[i]
 
     return clean_quat
-
-# Just for looking at the plots.
-#plot.figure()
-#plot.plot(clean_quat)
-#plot.show()
-
-# Add an empty row at the beginning so that np code in other scripts isnt broken. (skiprows = 1)
-# dummy_row = np.zeros((1,4))     
-# clean_quat = np.concatenate((dummy_row, clean_quat))
-# np.savetxt("sim_results/ref_att_quat.csv", clean_quat, fmt='%f', delimiter=',')     # Overwrite ref_att_quat.csv
